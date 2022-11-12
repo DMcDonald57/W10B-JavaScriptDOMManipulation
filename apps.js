91989d39965971282cc6b43db4b9d12e6e1d3803
DMcDonald57 <d.mcdonald579@gmail.com>
2022-11-12 16:17:26 -0700

    Completed the last selector by adding new tag before footer

--- apps.js
+++ apps.js
@@ -21,9 +21,16 @@ let pTags = document.querySelectorAll(`p`);
 let nestedTag = document.getElementById(`SubHeader`);
     nestedTag.innerHTML = `<a href ="tel:5552223434"><button>Call us</button></a>`;
 
-let newTag = document.getElementsByClassName(`footer`);
-    newTag.insertAdjacentHTML(`afterBegin`,`<p>This para comes before the footer!</p>`);
+// This is changing a paragraph to an  H6 header
 
 let changeTag = document.getElementById(`secondPara`);
-        changeTag.outerHTML = `<h6>This is now a Header!</h6>`;
+        changeTag.outerHTML = `<h6>"This is now a Header!"</h6>`;
+
+
+    // this is to add a P Tag before the footer header
+
+    let newTag = document.getElementsByClassName(`footer`);
+        for (let i = 0; i < newTag.length; i++) {
+            newTag[i].insertAdjacentHTML(`beforeBegin`, `<p>This para comes before the footer!</p>`)
+        }
 
