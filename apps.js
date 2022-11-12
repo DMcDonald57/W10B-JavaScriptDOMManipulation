b3b81fa2646b11f1f219dcbee2b09c809b3af91e
DMcDonald57 <d.mcdonald579@gmail.com>
2022-11-12 16:32:54 -0700

    Final push after cleaning up code

--- apps.js
+++ apps.js
@@ -1,5 +1,5 @@
-
 // changing body to background color
+
 let body = document.body;
     body.style.backgroundColor = `bisque`
 
@@ -21,16 +21,15 @@ let pTags = document.querySelectorAll(`p`);
 let nestedTag = document.getElementById(`SubHeader`);
     nestedTag.innerHTML = `<a href ="tel:5552223434"><button>Call us</button></a>`;
 
-// This is changing a paragraph to an  H6 header
+// This is changing a paragraph to an H6 header
 
 let changeTag = document.getElementById(`secondPara`);
         changeTag.outerHTML = `<h6>"This is now a Header!"</h6>`;
 
+// this is to add a P Tag before the footer header
 
-    // this is to add a P Tag before the footer header
-
-    let newTag = document.getElementsByClassName(`footer`);
-        for (let i = 0; i < newTag.length; i++) {
-            newTag[i].insertAdjacentHTML(`beforeBegin`, `<p>This para comes before the footer!</p>`)
+let newTag = document.getElementsByClassName(`footer`);
+    for (let i = 0; i < newTag.length; i++) {
+        newTag[i].insertAdjacentHTML(`beforeBegin`, `<p>This para comes before the footer!</p>`)
         }
 
